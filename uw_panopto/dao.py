6aad6608915d9e568b13c06633b9d8468cfd1174
mike seibel <mikes@washington.edu>
2020-02-19 16:18:50 -0800

    oauth2

--- uw_panopto/dao.py
+++ uw_panopto/dao.py
@@ -1,10 +1,33 @@
-import logging
-import os
 from os.path import abspath, dirname
 from restclients_core.dao import DAO
+from restclients_core.exceptions import DataFailureException
+import json
+import os
 
 
 class Panopto_DAO(DAO):
+    def __init__(self, *args, **kwargs):
+        super(Panopto_DAO, self).__init__(*args, **kwargs)
+
+        url = "{}/Panopto/oauth2/connect/token".format(
+            self.get_service_setting('HOST'))
+
+        auth = "Basic {}".format(
+            self.get_service_setting('AUTH_TOKEN'))
+
+        response = self.postURL(url, {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': auth
+        }, "grant_type=client_credentials&scope=api")
+
+        if response.status == 200:
+            self.access_token = json.loads(response.data)["access_token"]
+        else:
+            raise DataFailureException(
+                url, response.status,
+                json.loads(response.data)["error"] if (
+                    response.status == 400) else "Cannot get access token")
+
     def service_name(self):
         return 'panopto'
 
@@ -15,10 +38,6 @@ class Panopto_DAO(DAO):
         return self.get_implementation().is_mock()
 
     def _custom_headers(self, method, url, headers, body):
-        custom_headers = {}
-
-        token = self.get_service_setting('AUTH_TOKEN')
-        if token is not None:
-            custom_headers["Authorization"] = "Token {}".format(token)
-
-        return custom_headers
+        return {
+            "Authorization": "Bearer {}".format(self.access_token)
+        } if hasattr(self, 'access_token') else {}
